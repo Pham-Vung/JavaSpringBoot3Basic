4222289d21522e624e0309d8ec7a62cf2315d702
Vững Phạm Văn <vung0222166@huce.edu.vn>
2024-08-19 10:45:38 +0700

    Spring Security architecture, Authentication configuration with JWT

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -20,6 +20,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID) // UUID sẽ random ngẫu nhiên và không trùng lặp
     String id;
+
     String username;
     String password;
     String firstName;
